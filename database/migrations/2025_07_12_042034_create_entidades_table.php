26162cd70552b7d58c4423bb13e2454a1ba67c1e
George21G <150305777+George21G@users.noreply.github.com>
2025-07-12 19:00:59 -0500

    Se crean tablas de BD
    Se agregan las tablas de:
    
    usuarios biblioteca
    entidades
    prestamos
    libros

---
+++ database/migrations/2025_07_12_042034_create_entidades_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('entidades', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->enum('tipo', ['colegio', 'universidad', 'empresa']);
+            $table->string('direccion')->nullable();
+            $table->string('telefono')->nullable();
+            $table->string('email')->nullable();
+            $table->enum('estado', ['activo', 'inactivo'])->default('activo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entidades');
+    }
+};
