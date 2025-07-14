e5c9084608f78e3b1ee6e4854cfb50b33f108ab4
George21G <150305777+George21G@users.noreply.github.com>
2025-07-14 00:58:25 -0500

    actualización de apariencia

--- resources/js/pages/settings/appearance.tsx
+++ resources/js/pages/settings/appearance.tsx
@@ -1,4 +1,6 @@
 import { Head } from '@inertiajs/react';
+import AppearanceTabs from '@/components/appearance-tabs';
+import HeadingSmall from '@/components/heading-small';
 import { type BreadcrumbItem } from '@/types';
 import AppLayout from '@/layouts/app-layout';
 import SettingsLayout from '@/layouts/settings/layout';
@@ -14,8 +16,12 @@ export default function Appearance() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Appearance settings" />
+
             <SettingsLayout>
-                <div></div>
+                <div className="space-y-6">
+                    <HeadingSmall title="Appearance settings" description="Update your account's appearance settings" />
+                    <AppearanceTabs />
+                </div>
             </SettingsLayout>
         </AppLayout>
     );
